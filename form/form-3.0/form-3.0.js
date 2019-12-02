1f3988760037e431cc6edcf7cdee9a7413bb9de8
Raquel Oliveira <ana.santos@vibedesenv.com>
2019-12-02 18:12:36 -0300

    update form 2 e 3

--- form/form-3.0/form-3.0.js
+++ form/form-3.0/form-3.0.js
@@ -4,15 +4,8 @@
       .module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
       .controller('AppCtrl', function() {
         this.userState = '';
-        this.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-            'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-            'WY').split(' ').map(function (state) { return { abbrev: state }; });
+        this.states = ('AC AL AP AM BA CE DF ES GO MA MT MS MG PA PB PR PE PI RJ RN RS RO RR SC SP SE TO').split(' ').map(function (state) { return { abbrev: state }; });
       });
 })();
 
-
-/**
-Copyright 2018 Google LLC. All Rights Reserved. 
-Use of this source code is governed by an MIT-style license that can be found
-in the LICENSE file at http://material.angularjs.org/HEAD/license.
-**/
+              
