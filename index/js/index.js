82418a42523d623b96116c396232207e5ba96317
Raquel VIBE <ana.santos@vibetecnologia.com>
2024-03-02 16:41:12 -0300

    Revert "init projeto transformar o livro - cabala para o estudante - em um site"
    This reverts commit 3b06bc8a6c5870dbf802df7f7e49b4ab4942b7d2.

---
+++ index/js/index.js
@@ -0,0 +1,26 @@
+// VOLTAR AO TOPO
+$(document).ready(function() {
+    $('#subir').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+        return false;
+    });
+});
+
+//TABS PORTFÓLIO
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
